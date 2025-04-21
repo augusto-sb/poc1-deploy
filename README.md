13224d13a1889f1a0a5ddd05eb578c27bc9230e8
assert <assert@azure.com>
2025-04-21 14:18:29 -0300

    init: idea mock

---
+++ README.md
@@ -0,0 +1,3 @@
+this small repo clonse the other repos of the project, builds the containers and deploys everything
+
+you only need Docker for this
---
+++ script.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/augusto-sb/poc1-backend.git;
+
+git clone https://github.com/augusto-sb/poc1-frontend.git;
+
+docker compose -f poc1-compose.yml up --build;
