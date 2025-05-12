c2b9555eb43e00df3d3da2420abe5e57d571d05b
asaldivar <augustosaldivar@assertsolutions.com>
2025-05-12 12:19:12 -0300

    force-recreate

--- script.sh
+++ script.sh
@@ -14,4 +14,4 @@ done
 #git clone https://github.com/augusto-sb/poc1-backend.git;
 #git clone https://github.com/augusto-sb/poc1-frontend.git;
 
-docker compose -f poc1-compose.yml up --build;
+docker compose -f poc1-compose.yml up --build --force-recreate;
